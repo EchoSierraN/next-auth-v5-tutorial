d56f7151b2b068d9fe4dadd185ac2099df512b9b
Elias Shemsu <echosierran@gmail.com>
2024-06-24 09:26:07 +0300

    Signin button in nav bar

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        // pathname: "next-auth-v5-test1/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
--- src/auth.ts
+++ src/auth.ts
@@ -5,6 +5,9 @@ import Google from "next-auth/providers/google"
 import Github from "next-auth/providers/github"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    theme: {
+        logo: "/logo.png",
+    },
     adapter: PrismaAdapter(prisma),
     providers: [Google, Github],
 })
